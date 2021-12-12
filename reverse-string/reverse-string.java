47a097ecb272f04ecd9f4037dd739b8f8a6194d2
Priyanshu Vaishnavi <vaishnavipriyanshu198@gmail.com>
2021-12-12 18:31:12 +0530

    Time: 1 ms (95.37%), Space: 45.8 MB (74.46%) - LeetHub

---
+++ reverse-string/reverse-string.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int left = 0;
+   int end = s.length-1;
+   while (left < end) {
+      //swap
+      char temp = s[left];
+      s[left] = s[end];
+      s[end] = temp;
+      
+      left ++;
+      end --;
+    }
+}
+}
