55803147a4a30cf7ba3df01b7cb8ea2afb503a73
Priyanshu Vaishnavi <vaishnavipriyanshu198@gmail.com>
2022-01-15 23:58:55 +0530

    Time: 1 ms (25.86%), Space: 38.2 MB (6.06%) - LeetHub

---
+++ 476-number-complement/476-number-complement.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int findComplement(int num) {
+        var nBits = (int) Math.floor((Math.log(num) / Math.log(2)) + 1);
+	var mask = (1 << nBits) - 1;
+	return ~num & mask;
+
+    }
+}
