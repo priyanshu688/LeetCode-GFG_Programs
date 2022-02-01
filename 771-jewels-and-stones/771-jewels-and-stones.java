12089baed40bd04d0e40a38b2aea53397f784b82
Priyanshu Vaishnavi <vaishnavipriyanshu198@gmail.com>
2022-02-01 23:54:30 +0530

    Time: 2 ms (44.48%), Space: 41.8 MB (5.31%) - LeetHub

---
+++ 771-jewels-and-stones/771-jewels-and-stones.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+       int count=0;
+        for(int i=0;i<stones.length();i++){
+            for(int j=0;j<jewels.length();j++){
+                if(jewels.charAt(j)==stones.charAt(i)){
+                    count++;
+                }
+            }
+        }
+        return count;   
+    }
+}
