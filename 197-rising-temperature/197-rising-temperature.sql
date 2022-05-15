4c43872fb5d31af9067c12ee10d51117797765cb
Priyanshu Vaishnavi <vaishnavipriyanshu198@gmail.com>
2022-05-15 23:33:19 +0530

    Time: 403 ms (81.84%), Space: 0B (100.00%) - LeetHub

---
+++ 197-rising-temperature/197-rising-temperature.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+SELECT w2.id FROM Weather w1 INNER JOIN Weather w2 ON w1.recordDate  = DATE_SUB(w2.recordDate , INTERVAL 1 DAY) WHERE (w2.temperature - w1.temperature)>0;
